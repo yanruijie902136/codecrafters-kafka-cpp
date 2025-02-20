b8481e2535d8b1560dac23c758d04479b35ccf34
yanruijie902136 <yanruijie902136@gmail.com>
2025-02-20 09:52:43 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,56 @@
+#include <arpa/inet.h>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
-#include <netdb.h>
-#include <string>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <err.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    // Disable output buffering
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+static int create_server_socket() {
+        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_socket < 0) {
+                err(EXIT_FAILURE, "socket");
+        }
 
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        std::cerr << "Failed to create server socket: " << std::endl;
-        return 1;
-    }
+        const int reuse = 1;
+        if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+                err(EXIT_FAILURE, "setsockopt");
+        }
 
-    // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // ensures that we don't run into 'Address already in use' errors
-    int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        close(server_fd);
-        std::cerr << "setsockopt failed: " << std::endl;
-        return 1;
-    }
+        sockaddr_in server_addr;
+        std::memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        server_addr.sin_port = htons(9092);
 
-    struct sockaddr_in server_addr{};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(9092);
+        if (bind(server_socket, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) < 0) {
+                err(EXIT_FAILURE, "bind");
+        }
 
-    if (bind(server_fd, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) != 0) {
-        close(server_fd);
-        std::cerr << "Failed to bind to port 9092" << std::endl;
-        return 1;
-    }
+        const int connection_backlog = 5;
+        if (listen(server_socket, connection_backlog) < 0) {
+                err(EXIT_FAILURE, "listen");
+        }
 
-    int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-        close(server_fd);
-        std::cerr << "listen failed" << std::endl;
-        return 1;
-    }
+        return server_socket;
+}
 
-    std::cout << "Waiting for a client to connect...\n";
+static void handle_client(int client_socket) {
+        for ( ; ; ) {
 
-    struct sockaddr_in client_addr{};
-    socklen_t client_addr_len = sizeof(client_addr);
+        }
+}
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!\n";
-    
-    // Uncomment this block to pass the first stage
-    // 
-    // int client_fd = accept(server_fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len);
-    // std::cout << "Client connected\n";
-    // close(client_fd);
+int main(int argc, char *argv[]) {
+        int server_socket = create_server_socket();
+        for ( ; ; ) {
+                int client_socket = accept(server_socket, nullptr, nullptr);
+                if (client_socket < 0) {
+                        err(EXIT_FAILURE, "accept");
+                }
 
-    close(server_fd);
-    return 0;
+                std::thread thread(handle_client, client_socket);
+                thread.detach();
+        }
 }
